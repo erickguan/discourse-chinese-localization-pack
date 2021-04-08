89c0531dcc5293eed3eb33a5c50bddbb2ce83577
scavin <scavina@gmail.com>
2021-04-08 19:35:33 +0800

    Update bilibili.rb

--- lib/onebox/bilibili.rb
+++ lib/onebox/bilibili.rb
@@ -9,15 +9,14 @@ module Onebox
       # Try to get the video ID. Works for URLs of the form:
       # * http://www.bilibili.com/video/av4235068/
       def video_id
-        
         match = uri.path.match(/\/video\/(.*)(\.html)?.*/)
-
         return match[1] if match && match[1]
 
         nil
       rescue
         return nil
       end
+
 
       def to_html
         "<iframe src='https://player.bilibili.com/player.html?bvid=#{video_id}&high_quality=1' scrolling='no' border='0' frameborder='no' framespacing='0' width='100%' height='430' allowfullscreen='true'></iframe>"
